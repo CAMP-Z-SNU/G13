77f0492b38af6a3b0b56edfeb454d2f7283e0b32
jaswanth-design <jaswantharaveti8055@gmail.com>
2024-09-11 23:43:22 +0530

    Add files via upload

--- 4/array.c
+++ 4/array.c
@@ -1,9 +1,34 @@
-#include <stdio.h> 
+#include <stdio.h>
 
-int main() 
-{ 
-	int myArray[] = { 1, 2, 3, 4, 5 }; 
-	int arraySize = sizeof(myArray) / sizeof(myArray[0]); 
-	printf("The size of the array is: %d\n", arraySize); 
-    return 0; 
+#define SIZE 10  
+
+void findMaxMin(int arr[], int size, int *max, int *min) {
+    if (size <= 0) {
+        printf("Array size must be greater than 0.\n");
+        return;
+    }
+    
+    *max = arr[0];
+    *min = arr[0];
+    
+    for (int i = 1; i < size; i++) {
+        if (arr[i] > *max) {
+            *max = arr[i];
+        }
+        if (arr[i] < *min) {
+            *min = arr[i];
+        }
+    }
+}
+
+int main() {
+    int arr[SIZE] = {3, 5, 1, 9, 7, 6, 8, 4, 2, 0};  
+    int max, min;
+    
+    findMaxMin(arr, SIZE, &max, &min);
+    
+    printf("Maximum value: %d\n", max);
+    printf("Minimum value: %d\n", min);
+    
+    return 0;
 }
